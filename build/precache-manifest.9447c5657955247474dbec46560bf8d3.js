252a33d66c89e50c68a0a8c4a004f10b340ebc51
cruzhar <cruz_har@hotmail.es>
2020-07-24 11:27:43 -0600

    samrt

---
+++ build/precache-manifest.9447c5657955247474dbec46560bf8d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45c6ecf8fd6b5cda168b460b9b61bc82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dbf72c7f3115fedcf52",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b2c99b29e2b2a7b287b3",
+    "url": "/static/css/main.add189b7.chunk.css"
+  },
+  {
+    "revision": "3dbf72c7f3115fedcf52",
+    "url": "/static/js/2.f027b255.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f027b255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2c99b29e2b2a7b287b3",
+    "url": "/static/js/main.9eb101b2.chunk.js"
+  },
+  {
+    "revision": "4305ad129dcf89844c80",
+    "url": "/static/js/runtime-main.3ea18b6e.js"
+  }
+]);
